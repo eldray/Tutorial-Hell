05af938d88929acabea156c0c685477e4ce5bf63
eldray <83376852+eldray@users.noreply.github.com>
2023-04-27 03:15:32 +0000

    Create script.js

---
+++ Web Files/completesignup/script.js
@@ -0,0 +1,7 @@
+const regbox = document.querySelector('regbox');
+const loginlink = document.querySelector('login-link');
+const regitserlink = document.querySelector('register-link');
+
+registerlink.addEventListener('click', () => {
+    regbox.class List.add('active')
+});
