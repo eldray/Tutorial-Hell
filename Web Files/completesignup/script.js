673ad45ee1b9a86408d2865c3c52c7228c185da0
eldray <83376852+eldray@users.noreply.github.com>
2023-06-16 03:52:10 +0000

    sign up page

--- Web Files/completesignup/script.js
+++ Web Files/completesignup/script.js
@@ -1,6 +1,6 @@
-const regbox = document.querySelector('regbox');
-const loginlink = document.querySelector('login-link');
-const regitserlink = document.querySelector('register-link');
+const regbox = document.querySelector(".regbox");
+const loginlink = document.querySelector(".login-link");
+const regitserlink = document.querySelector(".register-link");
 
 registerlink.addEventListener('click', () => {
     regbox.classList.add('active')
