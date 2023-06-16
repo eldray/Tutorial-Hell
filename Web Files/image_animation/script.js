0a697994f13f6d4d95088f321877ca7da3437db5
eldray <83376852+eldray@users.noreply.github.com>
2023-06-16 03:53:24 +0000

    image animation completed

--- Web Files/image_animation/script.js
+++ Web Files/image_animation/script.js
@@ -1,7 +1,7 @@
 const track = document.getElementById("image-track");
 
 window.onmousedown = e => {
-    track.dataset mouseDownAt = e.clientx
+    track.dataset.mouseDownAt = e.clientx;
 };
 
 window.onmouseup = () => {
@@ -10,26 +10,27 @@ window.onmouseup = () => {
 }
 
 window.onmousemove = e => {
-        if (track.dataset.mouseDownAt === "0") return;
+    if (track.dataset.mouseDownAt === "0") return;
 
-        const mouseDelta = parseFloat(track.dataset.mouseDownAt) = e.ClientX,
-            maxDelta = window.innerWidth / 2;
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.ClientX,
+        maxDelta = window.innerWidth / 2;
 
-        const percentage = (mouseDelta / maxDelta) * -100,
-            nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+    const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
 
-        track.dataset.percentage = nextPercentage;
+    track.dataset.percentage = nextPercentage;
 
-        track.animate({
-            transform: 'translate(${nextPercentage}%, -50%)'
-        }, {
-            duration: 1200,
-            fill: "forwards"
-        });
+    track.animate({
+        transform: 'translate(${nextPercentage}%, -50%)'
+    }, {
+        duration: 1200,
+        fill: "forwards"
+    });
 
-        Image.animate({
-            objectPosition: '${100 + nextPercentage}% center'
-        }, {
-            duration: 1200,
-            fill: "forwards"
-        });
+    Image.animate({
+        objectPosition: '${100 + nextPercentage}% center'
+    }, {
+        duration: 1200,
+        fill: "forwards"
+    });
+}
