cf8a1b0108aef55e8ba49ec5d22cfa54cf08d259
eldray <83376852+eldray@users.noreply.github.com>
2023-06-24 01:47:52 +0000

    fix errors

--- Web Files/dubai_parallax_page/script.js
+++ Web Files/dubai_parallax_page/script.js
@@ -1,11 +1,13 @@
-let dubaitext = document.getElementbyId('dubaitext');
-let burjkhalifa = document.getElementbyId('burjkhalifa');
-let stars = document.getElementbyId('stars');
+(function(){
+    let dubaitext = document.getElementbyId('dubaitext');
+    let burjkhalifa = document.getElementbyId('burjkhalifa');
+    let stars = document.getElementbyId('stars');
 
-window.addEventListener('scroll', () => {
+    window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
     dubaiText.style.left = value * -2 + 'px';
     burjkhalifa.style.top = value * 1 + 'px';
     stars.style.top = value * 1 + 'px';
-});
+    });
+})();
